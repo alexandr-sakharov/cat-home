0a4cb014968b3afe56b3dfa382d2a24b11d0df43
saxar <alexandr_sakharov@internet.ru>
2022-12-30 00:31:54 +0300

    Промежуточный коммит

--- src/components/router/RoutesConfig.ts
+++ src/components/router/RoutesConfig.ts
@@ -1,6 +1,7 @@
 import ViewingCatsPage from "../pages/viewingPets/ViewingCatsPage";
 import ViewingPet from "@/components/pages/viewingPet/ViewingPet";
 import FavoritesPage from "@/components/pages/favorites/FavoritesPage";
+import DonationsPage from "@/components/pages/donation/DonationsPage";
 
 const routes = [
   {
@@ -13,6 +14,10 @@ const routes = [
   },
   {
     path: '/donation',
+    component: DonationsPage
+  },
+  {
+    path: '/favorites',
     component: FavoritesPage
   },
 ]
