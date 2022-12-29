2f2d3671d030e5e83f4596398ad7fca25e8257d0
saxar <alexandr_sakharov@internet.ru>
2022-12-29 20:37:51 +0300

    Промежуточный коммит

--- src/adapter/RequestAdapter.ts
+++ src/adapter/RequestAdapter.ts
@@ -2,44 +2,44 @@ import axios from 'axios'
 import config from '../data/config.json'
 
 enum ResponseCode {
-  OK = 'OK',
-  ERR = 'ERR',
-  UNAUTH = 'UNAUTH',
-  FORBIDDEN = 'FORBIDDEN',
+    OK = 'OK',
+    ERR = 'ERR',
+    UNAUTH = 'UNAUTH',
+    FORBIDDEN = 'FORBIDDEN',
 }
 
 enum ResponseCodeStatus {
-  UNAUTH = 401,
-  FORBIDDEN = 403
+    UNAUTH = 401,
+    FORBIDDEN = 403
 }
 
 interface JsonResponse<Type> {
-  code: ResponseCode
-  data: Type
-  message: string
+    code: ResponseCode
+    data: Type
+    message: string
 }
 
 const extractDataFromResponse = <ResponseType>(data: JsonResponse<ResponseType>): ResponseType | undefined => {
-  if (data.code === ResponseCode.ERR) {
-    throw new Error(data.message)
-  } else if (data.code === ResponseCode.FORBIDDEN) {
-    console.log('FORBIDDEN')
-  } else if (data.code === ResponseCode.UNAUTH) {
-    console.log('UNAUTH')
-  } else return data
+    if (data.code === ResponseCode.ERR) {
+        throw new Error(data.message)
+    } else if (data.code === ResponseCode.FORBIDDEN) {
+        console.log('FORBIDDEN')
+    } else if (data.code === ResponseCode.UNAUTH) {
+        console.log('UNAUTH')
+    } else return data
 }
 
 /**
  * @throws {Error}
  */
 const checkResponseError = (error: any): void => {
-  if (axios.isAxiosError(error)) {
-    if (error.response?.status === ResponseCodeStatus.UNAUTH) {
-      console.log('UNAUTH')
-    } else if (error.response?.status === ResponseCodeStatus.FORBIDDEN) {
-      console.log('FORBIDDEN')
+    if (axios.isAxiosError(error)) {
+        if (error.response?.status === ResponseCodeStatus.UNAUTH) {
+            console.log('UNAUTH')
+        } else if (error.response?.status === ResponseCodeStatus.FORBIDDEN) {
+            console.log('FORBIDDEN')
+        }
     }
-  }
 }
 
 const constructUrl = (relativeUrl: string): string => String(config.serverBaseUrl) + '/' + relativeUrl
@@ -48,55 +48,62 @@ const constructUrl = (relativeUrl: string): string => String(config.serverBaseUr
  * @throws {Error}
  */
 async function get<ResponseType>(relativeUrl: string): Promise<ResponseType | undefined> {
-  try {
-    const { data } = await axios.get<JsonResponse<ResponseType>>(
-      constructUrl(relativeUrl),
-      { withCredentials: true }
-    )
-    return extractDataFromResponse(data)
-  } catch (error) {
-    checkResponseError(error)
-    throw error
-  }
+    try {
+        const {data} = await axios.get<JsonResponse<ResponseType>>(
+            constructUrl(relativeUrl),
+            {withCredentials: true}
+        )
+        return extractDataFromResponse(data)
+    } catch (error) {
+        checkResponseError(error)
+        throw error
+    }
 }
 
 async function post<RequestBodyType, ResponseType>(relativeUrl: string, body: RequestBodyType): Promise<ResponseType | undefined> {
-  try {
-    const { data } = await axios.post<JsonResponse<ResponseType>>(
-      constructUrl(relativeUrl),
-      body,
-      { withCredentials: true }
-    )
-    return extractDataFromResponse(data)
-  } catch (error) {
-    checkResponseError(error)
-    throw error
-  }
+    const isAuth = {
+        'auth-token': 'rferfer' || localStorage.getItem('auth-token')
+    }
+    try {
+        const {data} = await axios.post<JsonResponse<ResponseType>>(
+            constructUrl(relativeUrl),
+            body,
+            {
+                withCredentials: true,
+                headers: isAuth,
+            }
+        )
+        return extractDataFromResponse(data)
+    } catch (error) {
+        checkResponseError(error)
+        throw error
+    }
 }
+
 async function put<RequestBodyType, ResponseType>(relativeUrl: string, body: RequestBodyType): Promise<ResponseType | undefined> {
-  try {
-    const { data } = await axios.put<JsonResponse<ResponseType>>(
-      constructUrl(relativeUrl),
-      body,
-      { withCredentials: true }
-    )
-    return extractDataFromResponse(data)
-  } catch (error) {
-    checkResponseError(error)
-    throw error
-  }
+    try {
+        const {data} = await axios.put<JsonResponse<ResponseType>>(
+            constructUrl(relativeUrl),
+            body,
+            {withCredentials: true}
+        )
+        return extractDataFromResponse(data)
+    } catch (error) {
+        checkResponseError(error)
+        throw error
+    }
 }
 
 async function del<ResponseType>(relativeUrl: string): Promise<ResponseType | undefined> {
-  try {
-    const { data } = await axios.delete<JsonResponse<ResponseType>>(
-      constructUrl(relativeUrl),
-      { withCredentials: true }
-    )
-    return extractDataFromResponse(data)
-  } catch (error) {
-    checkResponseError(error)
-  }
+    try {
+        const {data} = await axios.delete<JsonResponse<ResponseType>>(
+            constructUrl(relativeUrl),
+            {withCredentials: true}
+        )
+        return extractDataFromResponse(data)
+    } catch (error) {
+        checkResponseError(error)
+    }
 }
 
-export default { get, post, del, put }
+export default {get, post, del, put}
